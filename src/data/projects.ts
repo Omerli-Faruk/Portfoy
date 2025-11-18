19ae0bd0c010e58a3d32d5d160dde205d44f2706
Omerli-Faruk <avciakin35@gmail.com>
2025-11-18 21:58:34 +0300

    feat: Yeni proje eklendi

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,4 +1,6 @@
-import webSite from '../assets/photos/melihcetinSite.webp';
+import webSiteP from '../assets/photos/melihcetinSite.webp';
+import webSiteG from '../assets/photos/Geoguessr_clone.webp';
+
 
 // --- Data Structure ---
 export type Project = {
@@ -15,17 +17,18 @@ export const projects: Project[] = [
     {
         name: 'Proje 1: Portföy Sitesi',
         description: 'React ve Node.js kullanılarak geliştirilmiş, modern ve anlaşılır bir portföy sitesi',
-        image: webSite,
+        image: webSiteP,
         tags: ['React', 'Node.js', 'Tailwindcss'],
         github: 'https://github.com/Omerli-Faruk/portfolio-starter',
         live: 'https://www.melihcetin.dev',
     },
     {
-        name: 'Proje 2: XXX',
-        description: 'XXXXXXXX',
-        image: 'https://via.placeholder.com/400x250/cbd5e1/1e293b?text=Proje+Görseli',
-        tags: ['X', 'X', 'X'],
-        github: '#',
+        name: 'Proje 2: Geoguessr Clone',
+        description: 'Next.js ve Tailwind CSS ile geliştirilmiş, Mapillary Panorama API tabanlı, Haversine Formülü kullanarak mesafeye dayalı puanlama yapan, dinamik ve sürdürülebilir GeoGuessr tarzı konum tahmin oyunu.',
+        image: webSiteG,
+        tags: ['Next.js', 'TypeScript', 'Tailwindcss'],
+        github: 'https://github.com/Omerli-Faruk/geoguessr_clone',
+        live: 'https://geobleedat.vercel.app/'
     },
     {
         name: 'Proje 3: XXX',
